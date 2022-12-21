9f26a4c4efcb4d7e616b48883f16cb66bcafb714
TomTom <115710773+TomDfr@users.noreply.github.com>
2022-12-21 18:23:30 +0100

    Add files via upload

---
+++ portfolio/composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '19c43cc3b42e27485a9c22d525c97ea152dfa803',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '19c43cc3b42e27485a9c22d525c97ea152dfa803',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.7.1',
+      'version' => '6.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '49cd7ea3d2563f028d7811f06864a53b1f15ff55',
+    ),
+  ),
+);
